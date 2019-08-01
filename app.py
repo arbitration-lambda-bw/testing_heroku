f68d424a4a7fa4fff15a12e12c6d04dd2049c51b
cartman12 <mhd.ali.nasser@gmail.com>
2019-08-01 08:28:50 +0300

    fixing port

--- app.py
+++ app.py
@@ -80,7 +80,7 @@ def create_app():
 
   return app
 
-if __name__ == '__main__':
-    # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+if __name__ == "__main__":
+  app = create_app()
+  port = int(os.environ.get('PORT', 5000))
+  app.run(host='0.0.0.0', port=port)
